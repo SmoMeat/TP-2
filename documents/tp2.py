c5142ab5e9d72d1d3fe7607e35aba2ce7bf5c9bc
Mathieu <mathieuducharme86@gmail.com>
2024-04-29 21:15:31 -0400

    final fixs

--- documents/tp2.py
+++ documents/tp2.py
@@ -10,9 +10,8 @@ Afin de jouer, démarrer le serveur avec 'python serveur-web.py' et accéder ens
 à l'adresse 'localhost:8000/tp2.html'.
 
 @Date: 29 avril 2024
-@Author: Mathieu Ducharme
-@Contact: mathieu.ducharme@umontreal.ca
-@Matricule: 20297456
+@Authors: Mathieu Ducharme & Loïc Buisson-Fechter
+@Contacts: mathieu.ducharme@umontreal.ca & loic.buisson-fechter@umontreal.ca
 """
 
 import random
@@ -20,10 +19,17 @@ import time
 
 ROWS = 10
 COLS = 10
+RUN_UNIT_TEST = True
 
 def init():
     """Initialise la page web avec la grille qui listen aux clics de l'utilisateur.
     Est appelé dès que la page initiale est chargée"""
+    
+    # Active les tests unitaires
+    global RUN_UNIT_TEST
+    if RUN_UNIT_TEST:
+        run_tests()
+        RUN_UNIT_TEST = False
 
     # Établit la structure du html
     main = document.querySelector("#main")
@@ -32,7 +38,7 @@ def init():
         <div id="msg">Jouer!</div>
         <div id="errors">Erreurs: 0</div>
         <div id="coinsLeft"></div>
-        <div id="grid">""" + generate_html_grid() + """</div>
+        <div id="grid">""" + generate_html_grid(ROWS, COLS) + """</div>
     """
 
     # Affiche le message
@@ -56,24 +62,58 @@ def init():
     coins_left_element.innerHTML = 'Nombre de sous cachées: ' + str(coins_left)
 
 
+def run_tests():
+    """Execute les tests unitaires pour toutes les fonctions visées"""
+    test_randint()
+    test_generate_html_grid()
+    test_generate_2d_array()
+    test_generate_grid()
+    test_get_list_of_neighboring_tiles()
+    test_update_neighboring_tiles()
+
+
 def randint(a, b):
     """Retourne un entier au hasard entre a et b inclut"""
     return int(random.random() * (b - a + 1)) + a
 
 
-def generate_html_grid():
-    """Génère le html de la grille de jeu et la renvoie en string"""
+def test_randint():
+    """Test unitaire de la fonction randint()"""
+    assert randint(0, 0) == 0
+    assert randint(10, 10) == 10
+    assert randint(-3, -3) == -3
+    for _ in range(100):
+        assert 0 <= randint(0, 5) <= 5
+
+
+def generate_html_grid(rows, cols):
+    """Génère le html de la grille de jeu et la renvoie en string
+    Args:
+        rows (int): nombre de rangées de la matrice
+        cols (int): nombre de colonnes de la matrice
+    Returns:
+        (str): code html de la grille
+    """
 
     grid = ''
 
-    for i in range(ROWS):
+    for i in range(rows):
         row = ''
-        for j in range(COLS):
-                index  = i * ROWS + j
+        for j in range(cols):
+                index  = i * rows + j
                 row += '<td id="tile'+str(index)+'" onclick="clic('+str(index)+')"></td>'
         grid += '<tr>' + row + '</tr>'
 
     return '<table>' + grid + '</table>'
+
+
+def test_generate_html_grid():
+    """Test unitaire de la fonction generate_html_grid()"""
+    assert generate_html_grid(0, 0) == '<table></table>'
+    assert generate_html_grid(0, 3) == '<table></table>'
+    assert generate_html_grid(3, 0) == '<table><tr></tr><tr></tr><tr></tr></table>'
+    assert generate_html_grid(1, 1) == '<table><tr><td id="tile0" onclick="clic(0)"></td></tr></table>'
+    assert generate_html_grid(3, 3) == '<table><tr><td id="tile0" onclick="clic(0)"></td><td id="tile1" onclick="clic(1)"></td><td id="tile2" onclick="clic(2)"></td></tr><tr><td id="tile3" onclick="clic(3)"></td><td id="tile4" onclick="clic(4)"></td><td id="tile5" onclick="clic(5)"></td></tr><tr><td id="tile6" onclick="clic(6)"></td><td id="tile7" onclick="clic(7)"></td><td id="tile8" onclick="clic(8)"></td></tr></table>'
 
 
 def generate_2d_array(rows, cols, initial_value):
@@ -95,6 +135,15 @@ def generate_2d_array(rows, cols, initial_value):
         array_2d.append(row)
 
     return array_2d
+
+
+def test_generate_2d_array():
+    """Test unitaire de la fonction generate_2d_array()"""
+    assert generate_2d_array(0, 0, 0) == []
+    assert generate_2d_array(0, 1, 0) == []
+    assert generate_2d_array(1, 0, 0) == [[]]
+    assert generate_2d_array(1, 1, 987) == [[987]]
+    assert generate_2d_array(3, 3, '*') == [['*', '*', '*'], ['*', '*', '*'], ['*', '*', '*']]
 
 
 def generate_grid(rows, cols):
@@ -126,6 +175,17 @@ def generate_grid(rows, cols):
     return grid
 
 
+def test_generate_grid():
+    """Test unitaire de la fonction generate_grid()"""
+    assert generate_grid(0, 0) == []
+    assert generate_grid(0, 1) == []
+    assert generate_grid(1, 0) == [[]]
+    assert generate_grid(1, 1) == [[0]]
+    for _ in range(10):
+        generate_grid(10, 10)
+        assert 15 <= coins_amount <= 20
+
+
 def get_list_of_neighboring_tiles(x, y):
     """Renvoie une liste des coordonnées des cases voisines.
     Args:
@@ -139,6 +199,15 @@ def get_list_of_neighboring_tiles(x, y):
         (x-1, y),   (x, y),   (x+1, y),
         (x-1, y+1), (x, y+1), (x+1, y+1)
     ]
+
+
+def test_get_list_of_neighboring_tiles():
+    """Test unitaire de la fonction get_list_of_neighboring_tiles()"""
+    assert get_list_of_neighboring_tiles(1, 1) == [(0, 0), (1, 0), (2, 0), (0, 1), (1, 1), (2, 1), (0, 2), (1, 2), (2, 2)]
+    assert get_list_of_neighboring_tiles(0, 0) == [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+    assert get_list_of_neighboring_tiles(15, 15) == [(14, 14), (15, 14), (16, 14), (14, 15), (15, 15), (16, 15), (14, 16), (15, 16), (16, 16)]
+    assert get_list_of_neighboring_tiles(-9, -9) == [(-10, -10), (-9, -10), (-8, -10), (-10, -9), (-9, -9), (-8, -9), (-10, -8), (-9, -8), (-8, -8)]
+    assert get_list_of_neighboring_tiles(100, 100) == [(99, 99), (100, 99), (101, 99), (99, 100), (100, 100), (101, 100), (99, 101), (100, 101), (101, 101)]
 
 
 def update_neighboring_tiles(grid, x, y):
@@ -162,6 +231,14 @@ def update_neighboring_tiles(grid, x, y):
         grid[_x][_y] += 1
     
     return grid
+
+
+def test_update_neighboring_tiles():
+    """Test unitaire de la fonction update_neighboring_tiles()"""
+    assert update_neighboring_tiles([['*',0,0],[0,0,0],[0,0,0]], 0, 0) == [['*', 1, 0], [1, 1, 0], [0, 0, 0]]
+    assert update_neighboring_tiles([[0,0,0],[0,'*',0],[0,0,0]], 1, 1) == [[1, 1, 1], [1, '*', 1], [1, 1, 1]]
+    assert update_neighboring_tiles([['*','*',1],[2,'*',1],[0,0,0]], 1, 1) == [['*', '*', 2], [3, '*', 2], [1, 1, 1]]
+    assert update_neighboring_tiles([[0,0,1,'*',1],[2,2,2,1,1],['*','*',1,0,0],[3,'*',1,0,0],['*',1,0,0,0]], 3, 1) == [[0, 0, 1, '*', 1], [2, 2, 2, 1, 1], ['*', '*', 2, 0, 0], [4, '*', 2, 0, 0], ['*', 2, 1, 0, 0]]
 
 
 def update_html_grid():
